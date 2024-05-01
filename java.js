ec54cd9beb28abf04bc8b9eb31455122983a2e9f
Kartik Sahu <130122171+kartiksahu82@users.noreply.github.com>
2024-05-01 18:30:35 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,25 @@
+// Header Scroll
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 50) {
+        nav.classList.add("header-scrolled");
+    } else {
+        nav.classList.remove("header-scrolled");
+    }
+}
+
+
+var swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 1500,
+    },
+});
+
+
+
